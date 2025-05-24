110888fe32c0e1436eea85d3704c7f4c22ec1749
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2025-05-24 19:06:24 +0200

    fix: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,4 @@
-import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+import { configPkg } from '@adonisjs/eslint-config'
+export default configPkg({
+  ignores: ['coverage'],
+})
